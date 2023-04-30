0322bf12d0df93afa15d1fbb52dfcfbd1414c1d8
HappyReni <dnjsvlfwo@gmail.com>
2023-04-30 17:34:19 +0900

    refactored Anki class to make it more modular and readable

--- app.py
+++ app.py
@@ -9,13 +9,36 @@ import requests
 import os
 
 class Anki():
-    def __init__(self, csv_path, model_name):
+    def __init__(self, csv_path, model_name, deck_name):
+        if not os.path.isdir('audio'):
+            os.mkdir('audio')
+        
+        self.note_data = {  'word' : [],
+                            'definition' : [],
+                            'image_src' : [],
+                            'audio_src' : [],   }
+        
+        self.media_files = list()
+
+        self.driver= self._get_driver()
+        self.csv = self._read_csv(csv_path)
+        self.model = self._generate_model(model_name)
+        self.deck = self._generate_deck(deck_name)
+    
+    def __del__(self):
+        self.driver.quit()
+
+    def _get_driver(self):
         headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}
         chromedriver_filepath = chromedriver_autoinstaller.install()
-        self.driver = webdriver.Chrome(chromedriver_filepath)
-        self.csv = self._read_csv(csv_path)
-
-        self.model = genanki.Model(
+        driver = webdriver.Chrome(chromedriver_filepath)
+        return driver
+    def _get_css(self):
+        with open('style.css','r',encoding='utf-8') as c:
+            res = c.read()
+        return res
+    def _generate_model(self,model_name):
+        model = genanki.Model(
         random.randrange(1 << 30, 1 << 31),
         model_name,
         fields=[
@@ -40,10 +63,7 @@ class Anki():
 
                     <!-- 발음 사운드 -->
                     <div class="card-audio">
-                        <audio controls>
-                            <source src="{{Audio}}" type="audio/mpeg">
-                            Your browser does not support the audio element.
-                        </audio>
+                        {{Audio}}
                     </div>
                 </div>
                 ''',
@@ -57,15 +77,13 @@ class Anki():
             },
         ],
         css=self._get_css(),)
-
+        return model
+    def _generate_deck(self,deck_name):
         # Define the Anki deck
-        self.deck = genanki.Deck(
+        deck = genanki.Deck(
         random.randrange(1 << 30, 1 << 31),
-        'Spanish Vocabulary')
-    
-    def __del__(self):
-        self.driver.quit()
-
+        deck_name)
+        return deck
     def _read_csv(self,csv_path):
         '''
         Read a CSV file and return its rows as a list of dictionaries
@@ -83,43 +101,52 @@ class Anki():
         return rows
     
     def begin(self):
-        for row in self.csv:
+        for idx,row in enumerate(self.csv):
             word = row['word']
             definition_org = row['definition']
+
+            self.note_data['word'].append(word)
             try:
-                audio_src,definition = self.get_audio_and_def_url(word)
+                self._get_audio(word)
             except Exception as e:
                 print(e)
-                audio_src = ''
-                definition = definition_org
+                self.note_data['audio_src'].append('')
+                self.note_data['definition'].append(definition_org)
+
             try:
-                image_src = self.get_image_url(word)
+                self._get_image_url(word)
             except Exception as e:
                 print(e)
-                image_src = ''
+                self.note_data['image_src'].append('')
+
             # Add the data to the Anki deck
-            self.add_note_to_deck(self.deck, word, definition, image_src, audio_src)
-    
-    def write_to_apkg(self, file_name = "result"):
+            self._add_note_to_deck(self.note_data, idx)
+ 
+    def write_to_apkg(self, file_name = "anki_deck"):
         # Export the deck to an .apkg file
-        genanki.Package(self.deck).write_to_file(f'{file_name}.apkg')
+        package = genanki.Package(self.deck)
+        self._add_media_files(package)
+        package.write_to_file(f'{file_name}.apkg')
 
-    def _get_css(self):
-        with open('style.css','r',encoding='utf-8') as c:
-            res = c.read()
-        return res
-
-    def add_note_to_deck(self, deck, word, definition, image_url, audio_url):
+    def _add_note_to_deck(self, note_data, idx):
+        word = note_data['word'][idx]
+        definition = note_data['definition'][idx]
+        image_url = note_data['image_src'][idx]
+        audio_url = note_data['audio_src'][idx]
+        print(word,definition,image_url,audio_url)
         # Build the note
         note = genanki.Note(
             model=self.model,
-            fields=[word, definition, '<img src="{}">'.format(image_url), '[sound:{}]'.format(audio_url)],
+            fields=[word, definition, f'<img src="{image_url}">', f'[sound:{audio_url}]'],
             tags=['Vocabulary'])
 
         # Add the note to the deck
         self.deck.add_note(note)
+    
+    def _add_media_files(self,package):
+        package.media_files = self.media_files
 
-    def get_audio_and_def_url(self, word):
+    def _get_audio(self, word):
         word_url = f"https://dict.naver.com/eskodict/#/search?query={word}"
         # Navigate to the word URL
         self.driver.get(word_url)
@@ -127,8 +154,9 @@ class Anki():
         self.driver.implicitly_wait(10)
         # Find the audio element on the page
 
-        audio_path = f'audios/{word}.mp3'
-        
+        file = f'{word}.mp3'
+        audio_path = f'audio/{file}'
+
         if not os.path.isfile(audio_path):
             audio_element = self.driver.find_element(By.CSS_SELECTOR,'#searchPage_entry > div > div:nth-child(1) > div > span.unit_listen.my_old_pron_area > button')
             # Get the URL of the audio file
@@ -138,28 +166,29 @@ class Anki():
 
             with open(audio_path,'wb') as f:
                 f.write(response.content)
-            
-        definition = self.get_definition(word_url)
-        return os.path.abspath(audio_path), definition
+        
+        self.media_files.append(audio_path)
+        self.note_data['audio_src'].append(file)
+        self._get_definition(word_url)
 
-    def get_image_url(self, word):
+    def _get_image_url(self, word):
         url = f"https://pixabay.com/es/images/search/{word}/"
 
         self.driver.get(url)
         self.driver.implicitly_wait(10)
         img_element = self.driver.find_element(By.CSS_SELECTOR,'#app > div:nth-child(1) > div > div.container--wYO8e > div.results--mB75j > div > div > div:nth-child(1) > div:nth-child(1) > div > a > img')
         image_url = img_element.get_attribute('src')
-        return image_url
+        self.note_data['image_src'].append(image_url)
     
-    def get_definition(self,url):
+    def _get_definition(self,url):
         word = self.driver.find_element(By.CSS_SELECTOR,'#searchPage_entry > div > div:nth-child(1) > div > a')
         word.click()
         definition = self.driver.find_element(By.CSS_SELECTOR,'#content > div.section.section_entry._section_entry > div > div.entry_mean > div')
-        return definition.text
+        self.note_data['definition'].append(definition.text)
 
 
 if __name__=='__main__':
-    anki = Anki(csv_path='words.csv',model_name='Spanish Terms')
+    anki = Anki(csv_path='words.csv',model_name='Spanish Terms', deck_name='Spanish Vocabulary')
     anki.begin()
     anki.write_to_apkg()
 
